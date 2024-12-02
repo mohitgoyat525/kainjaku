49dad97fb5dabbc051d5758ce701e4692a9891f2
mohitgoyat525 <montyits71@gmail.com>
2024-12-02 10:16:04 +0530

    work-on-updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,11 +21,30 @@ timeLine.pause();
 
 navbarIcon.addEventListener("click", function () {
   timeLine.play();
+  document.body.style.overflow = "hidden";
+ 
 });
 
 cross.addEventListener("click", function () {
   timeLine.reverse();
+  document.body.style.overflow = "auto";
+
 });
+// --------------------drop down------------
+const selector = document.getElementById("languageSelector");
+const dropdown = document.getElementById("languageDropdown");
+
+selector.addEventListener("click", (e) => {
+  e.stopPropagation();
+  dropdown.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.add("hidden");
+  }
+});
+
 // ---------------------slider---------------------
 $(document).ready(function () {
   $('.slider').slick({
@@ -83,3 +102,9 @@ function toggleAccordion(element, forceOpen = false) {
     icon.style.transform = "rotate(0deg)";
   }
 }
+
+// Back to top button
+let topBtn = document.querySelector(".top-btn");
+
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+window.onscroll = () => topBtn.style.opacity = window.scrollY > 150 ? 1 : 0;
